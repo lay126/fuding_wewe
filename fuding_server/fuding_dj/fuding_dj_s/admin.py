a21db457f1d704011321c1e4adc126e4111ee30e
a young adelina Lee <lay126@gmail.com>
2015-07-30 18:42:12 +0900

    SERVER : DJANGO, USER MODELLING (STILL)

--- fuding_server/fuding_dj/fuding_dj_s/admin.py
+++ fuding_server/fuding_dj/fuding_dj_s/admin.py
@@ -11,3 +11,37 @@ from fuding_dj_s.models import *
 
 
 # Register your models here.
+
+class USERInline(admin.StackedInline):
+	model = USER
+	can_delete = False
+	verbose_name_plural = 'USER'
+
+class UserAdmin(admin.ModelAdmin):
+	inlines = (USERInline, )
+	list_display = ('id', 'username', 'email', 'password', )
+
+
+
+class USER_DATAAdmin(admin.ModelAdmin):
+	list_display = ('user_id',
+					'user_points',
+					'user_writes',
+					'user_likes', )
+
+class USER_POINTSAdmin(admin.ModelAdmin):
+	list_display = ('user_id',
+					'point_index',
+					'point_value',
+					'point_time', )
+
+
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+# Register your models here.
+admin.site.register(USER_DATA, USER_DATAAdmin)
+admin.site.register(USER_POINTS, USER_POINTSAdmin)
+
--- fuding_server/fuding_dj/fuding_dj_s/models.py
+++ fuding_server/fuding_dj/fuding_dj_s/models.py
@@ -4,13 +4,10 @@ from django import forms
 from django.db import models
 from django.conf import settings
 
+from datetime import *
+
 # this for mongo db 
-from mongoengine import *
-
-
-
-# Create your models here.
-
+# from mongoengine import *
 
 # # mongo db user
 # class USERS(EmbeddedDocument):
@@ -18,27 +15,48 @@ from mongoengine import *
 # 	user_pwd = stringField(max_length=50)
 
 
-# default user
+
+
+#--------------------------------------------------------------------
+#--- default user ---------------------------------------
+#--------------------------------------------------------------------
 class USER(models.Model):
 	class Meta:
 		verbose_name = u'USER'
 		db_table = 'USER_DB'
+	# user email, id are include here 
 	user = models.OneToOneField(settings.AUTH_USER_MODEL)
 
 class USER_DATA(models.Model):
 	class Meta:
 		verbose_name = u'USER_DATA'
 		db_table = 'USER_DATA_DB'
-	user_id = models.ForeignKey(User)
-	user_email = models.CharField(verbose_name=u'user_email', null=False, max_length=100)
-	user_name = models.CharField(verbose_name=u'user_name', null=False, max_length=10)
+	user_id = models.ForeignKey(USER)
 	user_points = models.IntegerField(verbose_name=u'user_points', default=0)
 	user_writes = models.IntegerField(verbose_name=u'user_writes', default=0)
 	# matching with write num
-	user_likes = models.CharField(verbose_name=u'user_likes', default=0)
+	user_likes = models.IntegerField(verbose_name=u'user_likes', default=0)
+
+class USER_POINTS(models.Model):
+	class Meta:
+		verbose_name = u'USER_POINTS'
+		db_table = 'USER_POINTS_DB'
+	user_id = models.ForeignKey(USER)
+	point_index = models.AutoField(verbose_name=u'point_index', primary_key=True, unique=True, db_index=True, )
+	point_value = models.IntegerField(verbose_name=u'point_value', default=0, )
+	point_time = models.DateTimeField(verbose_name=u'point_time', default=datetime.now, blank=True, )
+	# point_time = models.DateTimeField(verbose_name=u'point_time', auto_now_add=True, blank=True)
 
 
 
 
 
-	
+
+
+
+
+
+
+
+
+
