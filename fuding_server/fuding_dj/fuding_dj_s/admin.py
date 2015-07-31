ecc58360b14b069400f5ec99d54d7a318fea6aaa
a young adelina Lee <lay126@gmail.com>
2015-07-31 21:51:05 +0900

    SERVER : 디비 쪼매 만듬...

--- fuding_server/fuding_dj/fuding_dj_s/admin.py
+++ fuding_server/fuding_dj/fuding_dj_s/admin.py
@@ -35,6 +35,37 @@ class USER_POINTSAdmin(admin.ModelAdmin):
 					'point_value',
 					'point_time', )
 
+class USER_WRITESAdmin(admin.ModelAdmin):
+	list_display = ('user_id',
+					'write_index',
+					'write_value',
+					'write_time', )
+
+class USER_LIKESAdmin(admin.ModelAdmin):
+	list_display = ('user_id',
+					'like_index',
+					'like_value',
+					'like_time', )
+
+
+class WRITE_FRAMEAdmin(admin.ModelAdmin):
+	list_display = ('wf_index',
+					'wt_index',
+					'wc_index_1', 'wc_index_2', 'wc_index_3', 'wc_index_4',
+					'wc_index_5', 'wc_index_6', 'wc_index_7', 'wc_index_8', 'wc_index_9', 
+					'wc_total', 'wc_time', )
+
+class WRITE_TITLEAdmin(admin.ModelAdmin):
+	list_display = ('wt_index', 'wf_index', 
+					'user_id', 'wt_name', 'wt_ingre', 'wt_times', 'wt_tag', )
+
+class WRITE_CONTENTAdmin(admin.ModelAdmin):
+	list_display = ('wc_index', 'wf_index',
+					'wc_index_num', 'wc_img', 'wc_text', )
+
+
+
+
 
 
 # Re-register UserAdmin
@@ -44,4 +75,10 @@ admin.site.register(User, UserAdmin)
 # Register your models here.
 admin.site.register(USER_DATA, USER_DATAAdmin)
 admin.site.register(USER_POINTS, USER_POINTSAdmin)
+admin.site.register(USER_WRITES, USER_WRITESAdmin)
+admin.site.register(USER_LIKES, USER_LIKESAdmin)
+admin.site.register(WRITE_FRAME, WRITE_FRAMEAdmin)
+admin.site.register(WRITE_TITLE, WRITE_TITLEAdmin)
+admin.site.register(WRITE_CONTENT, WRITE_CONTENTAdmin)
+
 
--- fuding_server/fuding_dj/fuding_dj_s/models.py
+++ fuding_server/fuding_dj/fuding_dj_s/models.py
@@ -4,6 +4,7 @@ from django import forms
 from django.db import models
 from django.conf import settings
 
+from django.contrib.auth.models import User
 from datetime import *
 
 # this for mongo db 
@@ -31,7 +32,8 @@ class USER_DATA(models.Model):
 	class Meta:
 		verbose_name = u'USER_DATA'
 		db_table = 'USER_DATA_DB'
-	user_id = models.ForeignKey(USER)
+	# import from /django.contrib.auth.models/
+	user_id = models.ForeignKey(User)
 	user_points = models.IntegerField(verbose_name=u'user_points', default=0)
 	user_writes = models.IntegerField(verbose_name=u'user_writes', default=0)
 	# matching with write num
@@ -41,19 +43,83 @@ class USER_POINTS(models.Model):
 	class Meta:
 		verbose_name = u'USER_POINTS'
 		db_table = 'USER_POINTS_DB'
-	user_id = models.ForeignKey(USER)
+	user_id = models.ForeignKey(User)
 	point_index = models.AutoField(verbose_name=u'point_index', primary_key=True, unique=True, db_index=True, )
 	point_value = models.IntegerField(verbose_name=u'point_value', default=0, )
 	point_time = models.DateTimeField(verbose_name=u'point_time', default=datetime.now, blank=True, )
+	# point_time = models.DateTimeField(verbose_name=u'point_time', auto_now_add=True, blank=True)
+
+class USER_WRITES(models.Model):
+	class Meta:
+		verbose_name = u'USER_WRITES'
+		db_table = 'USER_WRITES_DB'
+	user_id = models.ForeignKey(USER)
+	write_index = models.AutoField(verbose_name=u'write_index', primary_key=True, unique=True, db_index=True, )
+	write_value = models.IntegerField(verbose_name=u'write_value', default=0, )
+	write_time = models.DateTimeField(verbose_name=u'write_time', default=datetime.now, blank=True, )
+	# point_time = models.DateTimeField(verbose_name=u'point_time', auto_now_add=True, blank=True)
+
+class USER_LIKES(models.Model):
+	class Meta:
+		verbose_name = u'USER_LIKES'
+		db_table = 'USER_LIKES_DB'
+	user_id = models.ForeignKey(USER)
+	like_index = models.AutoField(verbose_name=u'like_index', primary_key=True, unique=True, db_index=True, )
+	like_value = models.IntegerField(verbose_name=u'like_value', default=0, )
+	like_time = models.DateTimeField(verbose_name=u'like_time', default=datetime.now, blank=True, )
 	# point_time = models.DateTimeField(verbose_name=u'point_time', auto_now_add=True, blank=True)
 
 
 
 
 
+#--------------------------------------------------------------------
+#--- food write ---------------------------------------
+#--------------------------------------------------------------------
 
+class WRITE_FRAME(models.Model):
+	class Meta:
+		verbose_name = u'WRITE_FRAME'
+		db_table = 'WRITE_FRAME_DB'
+	wf_index = models.AutoField(verbose_name=u'wf_index', primary_key=True, db_index=True, )
+	wt_index = models.IntegerField(verbose_name=u'wt_index', null=False, default=0, )
+	wc_index_1 = models.IntegerField(verbose_name=u'wc_index_1', null=False, default=0, )
+	wc_index_2 = models.IntegerField(verbose_name=u'wc_index_2', null=False, default=0, )
+	wc_index_3 = models.IntegerField(verbose_name=u'wc_index_3', null=False, default=0, )
+	wc_index_4 = models.IntegerField(verbose_name=u'wc_index_4', null=False, default=0, )
+	wc_index_5 = models.IntegerField(verbose_name=u'wc_index_5', null=False, default=0, )
+	wc_index_6 = models.IntegerField(verbose_name=u'wc_index_6', null=False, default=0, )
+	wc_index_7 = models.IntegerField(verbose_name=u'wc_index_7', null=False, default=0, )
+	wc_index_8 = models.IntegerField(verbose_name=u'wc_index_8', null=False, default=0, )
+	wc_index_9 = models.IntegerField(verbose_name=u'wc_index_9', null=False, default=0, )
+	wc_total = models.IntegerField(verbose_name=u'wc_total', null=False, default=0, )
+	wc_time = models.DateTimeField(verbose_name=u'wc_time', default=datetime.now, blank=True, )
 
+class WRITE_TITLE(models.Model):
+	class Meta:
+		verbose_name = u'WRITE_TITLE'
+		db_table = 'WRITE_TITLE_DB'
+	wt_index = models.AutoField(verbose_name=u'wt_index', primary_key=True, db_index=True, )
+	# foreign
+	wf_index = models.IntegerField(verbose_name=u'wf_index', null=False, default=0, )
+	# foreign
+	user_id = models.IntegerField(verbose_name=u'user_id', null=False, default=0, )
+	wt_name = models.CharField(verbose_name=u'wt_name', max_length=20, null=False, default='no food name', )
+	# foreign
+	wt_ingre = models.IntegerField(verbose_name=u'wt_ingre',)
+	wt_times = models.IntegerField(verbose_name=u'wt_times', ) #소요시간(sec기준) 
+	# foreign
+	wt_tag = models.IntegerField(verbose_name=u'wt_tag',)
 
+class WRITE_CONTENT(models.Model):
+	class Meta:
+		verbose_name = u'WRITE_CONTENT'
+		db_table = 'WRITE_CONTENT_DB'
+	wc_index = models.AutoField(verbose_name=u'wc_index', primary_key=True, db_index=True, )
+	wf_index = models.IntegerField(verbose_name=u'wf_index', null=False, default=0, )
+	wc_index_num = models.IntegerField(verbose_name=u'wc_index_num', null=False, default=0, ) # 글에서 몇번째 카드인지를 명시 
+	wc_img = models.ImageField(verbose_name=u'wc_photo', upload_to='fuding_dj_s/images/content_img/', blank=True, )
+	wc_text = models.CharField(verbose_name=u'wc_text', max_length=200, )
 
 
 
