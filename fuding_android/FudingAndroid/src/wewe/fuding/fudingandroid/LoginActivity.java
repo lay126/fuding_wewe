50fbb5c0dd2ae26f6a8a8544fb340ac311fc84d2
Jeong Yeo-Eun <jye930217@gmail.com>
2015-08-06 04:17:36 +0900

    android:login .. 하는

--- fuding_android/FudingAndroid/src/wewe/fuding/fudingandroid/LoginActivity.java
+++ fuding_android/FudingAndroid/src/wewe/fuding/fudingandroid/LoginActivity.java
@@ -1,21 +1,28 @@
 package wewe.fuding.fudingandroid;
 
+import java.security.acl.Permission;
+
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.login.LoginResult;
+import com.facebook.LoggingBehavior;
 import com.facebook.login.widget.LoginButton;
+//import com.facebook.Session;
+//import com.facebook.SessionState;
+//import com.facebook.Settings;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity implements Permission {
 
 	CallbackManager callbackManager;
 
@@ -49,13 +56,58 @@ public class LoginActivity extends Activity {
 		// }
 		// });
 	}
-
-	public void SkipLoginOnClick(View view) {
-		if (view.getId() == R.id.skip_login_button) {
-			Intent intent = new Intent(LoginActivity.this,
-					FudingMainActivity.class);
-			startActivity(intent);
-		}
-	}
+	
+	// 다은이코드
+//	public void SkipLoginOnClick(View view) {
+//		if (view.getId() == R.id.skip_login_button) {
+//			Intent intent = new Intent(LoginActivity.this,
+//					FudingMainActivity.class);
+//			startActivity(intent);
+//		}
+//	}
+//
+//	private void facebookInit(Bundle savedInstanceState) {
+//		Settings.addLoggingBehavior(LoggingBehavior.INCLUDE_ACCESS_TOKENS);
+//
+//		Session session = Session.getActiveSession();
+//		if (session == null) {
+//			if (savedInstanceState != null) {
+//				session = Session.restoreSession(this, null, statusCallback,
+//						savedInstanceState);
+//				session.openForRead(new Session.OpenRequest(this)
+//						.setPermissions(
+//								Arrays.asList("public_profile", "read_stream",
+//										"user_likes", "user_birthday",
+//										"user_tagged_places")).setCallback(
+//								statusCallback));
+//				Log.d("savedInstanceState", "savedInstanceState");
+//			}
+//			if (session == null) {
+//				session = new Session(this);
+//				session.openForRead(new Session.OpenRequest(this)
+//						.setPermissions(
+//								Arrays.asList("public_profile", "read_stream",
+//										"user_likes", "user_birthday",
+//										"user_tagged_places", "email"))
+//						.setCallback(statusCallback));
+//				Log.d("savedInstanceState", "not savedInstanceState");
+//			}
+//
+//			Session.setActiveSession(session);
+//			// session.openForRead(new Session.OpenRequest(this)
+//			// .setPermissions(
+//			// Arrays.asList("public_profile", "read_stream",
+//			// "user_likes", "user_birthday",
+//			// "user_tagged_places")).setCallback(
+//			// statusCallback));
+//
+//			if (session.getState().equals(SessionState.CREATED_TOKEN_LOADED)) {
+//				session.openForRead(new Session.OpenRequest(this)
+//						.setCallback(statusCallback));
+//			}
+//		}
+//
+//		updateView();
+//	}
 
 }
