4cec7756c420abcdb608ad4b59c74933ca4f9d0a
yundaeun <dbsekdbsek0@gmail.com>
2015-08-22 02:30:53 +0900

    android :  뉴스피드 상세페이지

---
+++ fuding_android/FudingAndroid/src/wewe/fuding/activity/DetailActivity.java
@@ -0,0 +1,110 @@
+package wewe.fuding.activity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import wewe.fuding.domain.Frame;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.Request.Method;
+import com.android.volley.Response.ErrorListener;
+import com.android.volley.Response.Listener;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class DetailActivity extends Activity {
+
+	private ArrayList<Frame> frameArr;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    
+        getAllContentDetail(frameArr);
+    }
+
+	private void getAllContentDetail(ArrayList<Frame> frameArr) { 
+			String URL_address = "http://119.205.252.224:8000/get/recipe/";
+
+			RequestQueue mQueue;
+			mQueue = Volley.newRequestQueue(this);
+
+			Listener<String> listener = new Listener<String>() {
+				@Override
+				public void onResponse(String response) {
+					// to make data available
+					String arrRes = "{'response':" + response + "}";
+					//Log.d(TAG, arrRes);
+
+					JSONObject jobject = null;
+					try {
+						jobject = new JSONObject(arrRes);
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
+
+					JSONArray jarray = null;
+					try {
+						jarray = jobject.getJSONArray("response");
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
+
+					try {
+						for (int i = 0; i < jarray.length(); i++) {
+							JSONObject jsonFrame = (JSONObject) jarray.get(i);
+
+//							nfFrame = new Frame();
+//
+//							nfFrame.setUserId(jsonFrame.getString("wt_name"));
+//							nfFrame.setWriteDate(jsonFrame.getString("wc_date"));
+//							nfFrame.setLikeCnt(Integer.parseInt(jsonFrame.getString("wf_likes")));
+//							nfFrame.setTag(jsonFrame.getString("wt_tag"));
+//							nfFrame.setFoodImgURL(jsonFrame.getString("wc_img"));
+//							
+//							frameArr.add(nfFrame);
+						}
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
+				}
+			};
+
+			ErrorListener errorListener = new com.android.volley.Response.ErrorListener() {
+				@Override
+				public void onErrorResponse(VolleyError error) {
+					error.printStackTrace();
+					Toast.makeText(DetailActivity.this, "상세페이지 : 네트워크상태가좋지 않습니다.잠시만 기다려주세요.",
+							Toast.LENGTH_LONG).show();
+				}
+			};
+
+			StringRequest req = new StringRequest(Method.POST, URL_address,
+					listener, errorListener) {
+				@Override
+				protected Map<String, String> getParams() throws AuthFailureError {
+					Map<String, String> params = new HashMap<String, String>();
+					params.put("user_name", "ayoung");
+					return params;
+				}
+
+			};
+
+			mQueue.add(req); 
+	}
+
+}
