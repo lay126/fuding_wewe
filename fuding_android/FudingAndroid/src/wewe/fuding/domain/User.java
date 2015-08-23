3d12a596aaa5b39d7cbaa95d85163f6a693581eb
Jeong Yeo-Eun <jye930217@gmail.com>
2015-08-23 22:41:54 +0900

    android:UserPhoto 필드 추가 #16

--- fuding_android/FudingAndroid/src/wewe/fuding/domain/User.java
+++ fuding_android/FudingAndroid/src/wewe/fuding/domain/User.java
@@ -1,14 +1,28 @@
 package wewe.fuding.domain;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class User {
 	private int userNum;
 	private String userEmail;
 	private String userPwd;
 	private String userId;
 	private String userName;
+	private String userPhoto;
 
 	public User() {
 		super();
+	}
+
+	public User(int userNum, String userEmail, String userPwd, String userId,
+			String userName, String userPhoto) {
+		this.userNum = userNum;
+		this.userEmail = userEmail;
+		this.userPwd = userPwd;
+		this.userId = userId;
+		this.userName = userName;
+		this.userPhoto = userPhoto;
 	}
 
 	public int getUserNum() {
@@ -51,4 +65,18 @@ public class User {
 		this.userName = userName;
 	}
 
+	public String getUserPhoto() {
+		return userPhoto;
+	}
+
+	public void setUserPhoto(String userPhoto) {
+		this.userPhoto = userPhoto;
+	}
+
+	public static User fromJson(JSONObject object) throws JSONException {
+		return new User(object.getInt("userNum"),
+				object.getString("userEmail"), object.getString("userPwd"),
+				object.getString("userId"), object.getString("userName"),
+				object.getString("userPhoto"));
+	}
 }
