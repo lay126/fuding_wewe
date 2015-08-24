f5622382bf4feefb8272f3c7630837168515e346
Jeong Yeo-Eun <jye930217@gmail.com>
2015-08-25 01:28:01 +0900

    android: follow list activity 만들어놓음

---
+++ fuding_android/FudingAndroid/src/wewe/fuding/activity/FollowerListActivity.java
@@ -0,0 +1,16 @@
+package wewe.fuding.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class FollowerListActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_follower_list);
+	}
+
+}
